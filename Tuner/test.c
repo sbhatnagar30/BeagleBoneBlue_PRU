b1e4644ff79bc73f5ebc1b40333b60631de74d70
Samarthya Bhatnagar <sbhatnagar30>
2018-12-11 18:16:44 +0000

    tuning

--- Tuner/test.c
+++ Tuner/test.c
@@ -12,7 +12,9 @@ double angular_pos = 0;
 double v_motor_log[2000];
 double angular_pos_log[2000];
 int fast = 0;
+int medium = 0;
 int slow = 0;
+int flag = 1;
 
 static int running = 0;
 // interrupt handler to catch ctrl-c
@@ -36,45 +38,55 @@ int main()
   signal(SIGINT, __signal_handler);
   running=1;
   while(running){
-    if (fast % 2000 == 0) { //should execute at 1 Khz
-      //controller implementation
+    if (fast % 100 == 0) { //should execute at 1 Khz
       angular_pos = rc_encoder_eqep_read(1) * 2*pi/1920.0;
-      if (slow % 4 == 0) {
-        v_motor = 10;
+      if (medium % 200 == 0) {
+        //controller implementation
+        if (slow % 4 == 0) {
+          v_motor = 10;
+        }
+        if (slow % 4 == 1) {
+          v_motor = 0;
+        }
+        if (slow % 4 == 2) {
+          v_motor = -10;
+        }
+        if (slow % 4 == 3) {
+          v_motor = 0;
+        }
+        duty_cycle = (v_motor/12.0);
+        //printf("%f\n",duty_cycle);
+        rc_motor_set(1,duty_cycle);
+        slow++;
+        medium = 0;
       }
-      if (slow % 4 == 1) {
-        v_motor = 0;
+      if (slow < 10) {
+        v_motor_log[200*slow + medium] = v_motor;
+        angular_pos_log[200*slow + medium] = angular_pos;
       }
-      if (slow % 4 == 2) {
-        v_motor = -10;
+      if (slow == 10 && flag == 1) {
+        printf("writing");
+        FILE *fp;
+        fp = fopen("data.txt", "w+");
+        int i;
+        fprintf(fp,"---------------\n");
+        for (i = 0; i < 2000; i++) {
+          fprintf(fp,"%f\n",v_motor_log[i]);
+        }
+        fprintf(fp,"---------------\n");
+        for (i = 0; i < 2000; i++) {
+          fprintf(fp,"%f\n",angular_pos_log[i]);
+        }
+        fprintf(fp,"---------------\n");
+        flag = 0;
+        fclose(fp);
+        printf("done");
       }
-      if (slow % 4 == 3) {
-        v_motor = 0;
-      }
-      duty_cycle = (v_motor/12.0);
-      //printf("%f\n",duty_cycle);
-      //rc_motor_set(1,duty_cycle);
-      slow++;
+      medium++;
       fast = 0;
     }
-    if (slow < 1) {
-      v_motor_log[2000*slow + fast] = v_motor;
-      angular_pos_log[2000*slow + fast] = angular_pos;
-    }
-    if (slow == 1) {
-      int i;
-      printf("---------------\n");
-      for (i = 0; i < 2000; i++) {
-        printf("%f\n",v_motor_log[i]);
-      }
-      printf("---------------\n");
-      for (i = 0; i < 2000; i++) {
-        printf("%f\n",angular_pos_log[i]);
-      }
-      printf("---------------\n");
-    }
     fast++;
-    rc_usleep(10);
+    rc_usleep(1);
   }
   rc_motor_cleanup();
   rc_encoder_eqep_cleanup();
